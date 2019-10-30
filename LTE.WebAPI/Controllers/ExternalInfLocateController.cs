033310fcd3214a0fce4e37d1acd8db7ce60f429f
Ssxy0606 <13269486988@163.com>
2019-10-30 10:47:40 +0800

    整合网外干扰定位相关代码

---
+++ LTE.WebAPI/Controllers/ExternalInfLocateController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LTE.WebAPI.Models;
+namespace LTE.WebAPI.Controllers
+{
+    public class ExternalInfLocateController : ApiController
+    {
+        [HttpPost]
+        public Result LocateAnalysis([FromBody]ExternalInfLocateModel pa)
+        {
+            return pa.LocateByPath();
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
