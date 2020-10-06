1556fb39a599c1aa66bc9f2eeb40b4c99d1946de
ShengXiangXiong <823416203@qq.com>
2020-10-06 16:40:21 +0800

    add mq

---
+++ LTE/Utils/RedisMq.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTE.Utils
+{
+    public class RedisMq
+    {
+        static IDatabase db = RedisHelper.getInstance().db;
+        public static ISubscriber subscriber = db.Multiplexer.GetSubscriber();
+        public static long Pub(RedisChannel channel, RedisValue message)
+        {
+            return db.Publish(channel, message);
+        }
+    }
+}
